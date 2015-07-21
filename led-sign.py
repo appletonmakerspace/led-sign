f235c947fb61d37e869160d3a6ea8b7c8871e321
Mark Finn <mark@dockstar.dhmn.net>
2015-07-21 15:43:01 -0500

    use multiple TEXTs on the alphabrite, randomize them. makes each message display only once instead of timing it. also went to a rendom transition, not sure about it.

--- led-sign.py
+++ led-sign.py
@@ -2,54 +2,69 @@ import alphasign
 import sys
 import time
 import urllib2
+import random
+
+def labelgen():
+  l=32
+  while l < 0x7f:
+    yield chr(l)
+    l+=1
+    if l==0x30:
+      l=0x36  
+    if l==0x3f:
+      l=0x40 
+
+
 
 def update_sign():
 
         sign = alphasign.Serial(device='/dev/ttyUSB0')
         sign.connect()
-        sign.clear_memory()
-
-        # create an empty alphasign.String
-        alpha_str = alphasign.String(size=64)
-
-        # create a single alphasign.Text object with a placeholder for our alphasign.String
-        alpha_txt = alphasign.Text(alpha_str.call(),mode=alphasign.modes.COMPRESSED_ROTATE)
-
-        # allocate memory for these objects on the sign
-        sign.allocate((alpha_str,alpha_txt))
-
-        # tell sign to only display the text part
-        sign.set_run_sequence((alpha_txt,))
-
-        # write objects
-        for obj in (alpha_str,alpha_txt):
-            sign.write(obj)
-
-        # This gives time for the serial write to complete before we issue the next write.
-        time.sleep(5)
-
+        time.sleep(1)
         last_payload = ''
 
         while True:
-
+            success=False
+            
             try:
                 # fetch new-line separated plain text from Interweb
                 payload = urllib2.urlopen("https://server.appletonmakerspace.org/wiki/doku.php?id=sign_text&do=export_raw").read()
+                success=True
             except Exception as e:
                 payload = 'net err: '+str(e)
 
             # alert bystanders with beep when we update
-            if payload != last_payload:
+            if payload == last_payload:
+                t=txts[:]
+                random.shuffle(t)
+                sign.set_run_sequence(t)
+            else:
                 sign.beep(frequency=20, duration=1)
                 last_payload = payload
 
-            # loop over each line of plain text, displaying for 10 seconds
-            for line in payload.splitlines():
-                alpha_str.data = line
-                sign.write(alpha_str)
-                time.sleep(10)
+                # loop over each line of plain text, displaying for 10 seconds
+                txts=[]
+                strs=[]
+                l=labelgen()
+                for line in payload.splitlines():
+                    line = line.strip()
+                    if not line:
+                       continue
+#                str = alphasign.String(line)
+#                strs.append(str)
+                    txts.append(alphasign.Text(line,mode=alphasign.modes.AUTOMODE, label=next(l)))
 
-            # when the last line is read, the while loop repeats and fetches again
+                sign.clear_memory()
+                sign.allocate(txts+strs)
+                for obj in txts + strs:
+                    sign.write(obj)
+
+                sign.set_run_sequence(txts[:])
+                
+                                
+            time.sleep(10)
+            if success:
+                time.sleep(110)
 
 if __name__ == "__main__":
     update_sign()
